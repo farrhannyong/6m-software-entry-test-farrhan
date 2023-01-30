7c6e7c90b223f43423c3dc5797b2fa159dcc4435
Farrhan <103014061+farrhannyong@users.noreply.github.com>
2023-01-31 01:07:06 +0800

    Update q5.js

--- q5.js
+++ q5.js
@@ -6,5 +6,11 @@
 */
 
 // Task 1: Add code here
+function printSomething(){
+    console.log("I am printing something.");
+}
+
+module.exports.print = printSomething;
+const { print } = require('./moduleName');
 
 print();
